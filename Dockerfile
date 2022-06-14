d7d132e096bba72a254d039462ca0d48d4d1a1a0
Enrique Garcia <garcia@lapp.in2p3.fr>
2022-06-14 15:45:52 +0200

    fix Ci with image name

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM docker.io/python:3.8-slim
 
 SHELL [ "/bin/bash", "-c"]
 
-RUN apt update &&  apt-get install -y git
+RUN apt update
 COPY . test_github_actions
 RUN pip install "./test_github_actions"
 RUN rm -rf test_github_actions
--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
 import setuptools
+import test_github_actions
 
 # Define entry points for command-line scripts
+entry_points = {'console_scripts': ['square_number = test_github_actions.test:main']}
+
 
 setuptools.setup(name='test_actions',
                  version='v0.1',
@@ -11,4 +14,5 @@ setuptools.setup(name='test_actions',
                  tests_require=['pytest'],
                  author='https://github.com/garciagenrique',
                  author_email='garcia<at>lapp.in2p3.fr',
+                 entry_points=entry_points,
                  )  
